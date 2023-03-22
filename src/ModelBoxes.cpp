745fa37a47258857034cb41cf83f3b8e0e0c4ece
Yury Bobylev <bobilev_yury@mail.ru>
2023-03-22 16:00:29 +0400

    notes

--- src/ModelBoxes.cpp
+++ src/ModelBoxes.cpp
@@ -1,8 +1,18 @@
 /*
- * ModelBoxes.cpp
- *
- *  Created on: 19 мар. 2023 г.
- *      Author: yury
+ Copyright 2022-2023 Yury Bobylev <bobilev_yury@mail.ru>
+
+ This file is part of MyLibrary.
+ MyLibrary is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+ MyLibrary is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with MyLibrary. If not,
+ see <https://www.gnu.org/licenses/>.
  */
 
 #include <ModelBoxes.h>
